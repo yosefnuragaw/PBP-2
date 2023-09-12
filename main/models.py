053b534e136940b295f33047ee20f3f1aedc0b96
Yosef <yosefnuragaw@gmail.com>
2023-09-12 18:01:37 +0700

    Add Tugas 2 requirement

--- main/models.py
+++ main/models.py
@@ -7,4 +7,5 @@ class Product(models.Model):
     name = models.CharField(max_length=255)
     date_added = models.DateField(auto_now_add=True)
     price = models.IntegerField()
+    amount = models.IntegerField()
     description = models.TextField()
