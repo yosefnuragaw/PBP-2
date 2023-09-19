1c330ac863a86ee8643b03e7463f6275225bbb70
Yosef <yosefnuragaw@gmail.com>
2023-09-20 01:05:00 +0700

    Submission Tugas 3

--- main/models.py
+++ main/models.py
@@ -5,7 +5,6 @@ from django.db import models
 
 class Product(models.Model):
     name = models.CharField(max_length=255)
-    date_added = models.DateField(auto_now_add=True)
     price = models.IntegerField()
-    amount = models.IntegerField()
     description = models.TextField()
+    date_added = models.DateField(auto_now_add=True)
